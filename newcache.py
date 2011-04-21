f0121e9f888be013dc72f20fb41c66ca9110e2ad
Mike Tigas <mike@tig.as>
2011-04-21 16:42:21 -0700

    Major refactor for Django 1.3+. Reduces redundancies between the original module and the new (Django 1.3+) built-in support for versioning, key prefixes, and PylibMC.

--- newcache.py
+++ newcache.py
@@ -1,38 +1,13 @@
-"Modified memcached cache backend"
-
+"""
+Django 1.3+ refactor of `newcache` that reduces redundancies between
+the original module and the new (Django 1.3+) built-in support for
+versioning, key prefixes, and PylibMC.
+"""
 import time
 
-from threading import local
-
-from django.core.cache.backends.base import BaseCache, InvalidCacheBackendError
-from django.utils.hashcompat import md5_constructor
+from django.core.cache.backends.memcached import BaseMemcachedCache, MemcachedCache, PyLibMCCache
 from django.conf import settings
 
-try:
-    from django.utils import importlib
-except ImportError:
-    import importlib
-
-try:
-    import pylibmc as memcache
-    NotFoundError = memcache.NotFound
-    using_pylibmc = True
-except ImportError:
-    using_pylibmc = False
-    try:
-        import memcache
-        NotFoundError = ValueError
-    except ImportError:
-        raise InvalidCacheBackendError('Memcached cache backend requires ' + 
-            'either the "pylibmc" or "memcache" library')
-
-# Flavor is used amongst multiple apps to differentiate the "flavor" of the
-# environment. Examples of flavors are 'prod', 'staging', 'dev', and 'test'.
-FLAVOR = getattr(settings, 'FLAVOR', '')
-
-CACHE_VERSION = str(getattr(settings, 'CACHE_VERSION', 1))
-CACHE_BEHAVIORS = getattr(settings, 'CACHE_BEHAVIORS', {'hash': 'crc'})
-CACHE_KEY_MODULE = getattr(settings, 'CACHE_KEY_MODULE', 'newcache')
 CACHE_HERD_TIMEOUT = getattr(settings, 'CACHE_HERD_TIMEOUT', 60)
 
 class Marker(object):
@@ -40,53 +15,13 @@ class Marker(object):
 
 MARKER = Marker()
 
-def get_key(cachename, flavor=FLAVOR, version=CACHE_VERSION):
+class BaseNewCache(BaseMemcachedCache):
     """
-    Returns a hashed, versioned, flavored version of the string that was input.
+    Base class that adds "thundering herd" mitigation to the Django 1.3+
+    memcached cache classes.
+    
+    Use one of the subclasses below.
     """
-    if type(cachename) == unicode:
-        key = cachename.encode('utf-8')
-    else:
-        key = cachename
-    
-    hashed = md5_constructor(key).hexdigest()
-    return ''.join((flavor, '-', str(version), '-', hashed))
-
-KEY_FUNC = importlib.import_module(CACHE_KEY_MODULE).get_key
-
-class CacheClass(BaseCache):
-
-    def __init__(self, server, params, key_prefix=FLAVOR, version=CACHE_VERSION, key_func=None):
-        if not key_func:
-            key_func = KEY_FUNC
-        
-        super(CacheClass, self).__init__(params, key_prefix, version, key_func)
-        self._servers = server.split(';')
-        self._use_binary = bool(params.get('binary'))
-        self._local = local()
-    
-    @property
-    def _cache(self):
-        """
-        Implements transparent thread-safe access to a memcached client.
-        """
-        client = getattr(self._local, 'client', None)
-        if client:
-            return client
-        
-        # Use binary mode if it's both supported and requested
-        if using_pylibmc and self._use_binary:
-            client = memcache.Client(self._servers, binary=True)
-        else:
-            client = memcache.Client(self._servers)
-        
-        # If we're using pylibmc, set the behaviors according to settings
-        if using_pylibmc:
-            client.behaviors = CACHE_BEHAVIORS
-        
-        self._local.client = client
-        return client
-
     def _pack_value(self, value, timeout):
         """
         Packs a value to include a marker (to indicate that it's a packed
@@ -94,7 +29,7 @@ class CacheClass(BaseCache):
         """
         herd_timeout = (timeout or self.default_timeout) + int(time.time())
         return (MARKER, value, herd_timeout)
-    
+
     def _unpack_value(self, value, default=None):
         """
         Unpacks a value and returns a tuple whose first element is the value,
@@ -110,24 +45,7 @@ class CacheClass(BaseCache):
             return unpacked, True
         return unpacked, False
 
-    def _get_memcache_timeout(self, timeout):
-        """
-        Memcached deals with long (> 30 days) timeouts in a special
-        way. Call this function to obtain a safe value for your timeout.
-        """
-        if timeout is None:
-            timeout = self.default_timeout
-        if timeout > 2592000: # 60*60*24*30, 30 days
-            # See http://code.google.com/p/memcached/wiki/FAQ
-            # "You can set expire times up to 30 days in the future. After that
-            # memcached interprets it as a date, and will expire the item after
-            # said date. This is a simple (but obscure) mechanic."
-            #
-            # This means that we have to switch to absolute timestamps.
-            timeout += int(time.time())
-        return timeout
-
-    def add(self, key, value, timeout=None, version=None, herd=True):
+    def add(self, key, value, timeout=0, version=None, herd=True):
         # If the user chooses to use the herd mechanism, then encode some
         # timestamp information into the object to be persisted into memcached
         if herd and timeout != 0:
@@ -139,27 +57,29 @@ class CacheClass(BaseCache):
         else:
             packed = value
             real_timeout = self._get_memcache_timeout(timeout)
-        return self._cache.add(self.make_key(key, version=version), packed, real_timeout)
+
+        key = self.make_key(key, version=version)
+        return self._cache.add(key, packed, real_timeout)
 
     def get(self, key, default=None, version=None):
-        encoded_key = self.make_key(key, version=version)
+        key = self.make_key(key, version=version)
         packed = self._cache.get(encoded_key)
         if packed is None:
             return default
-        
+
         val, refresh = self._unpack_value(packed)
-        
+
         # If the cache has expired according to the embedded timeout, then
         # shove it back into the cache for a while, but act as if it was a
         # cache miss.
         if refresh:
-            self._cache.set(encoded_key, val,
+            self._cache.set(key, val,
                 self._get_memcache_timeout(CACHE_HERD_TIMEOUT))
             return default
-        
+
         return val
 
-    def set(self, key, value, timeout=None, version=None, herd=True):
+    def set(self, key, value, timeout=0, version=None, herd=True):
         # If the user chooses to use the herd mechanism, then encode some
         # timestamp information into the object to be persisted into memcached
         if herd and timeout != 0:
@@ -171,60 +91,82 @@ class CacheClass(BaseCache):
         else:
             packed = value
             real_timeout = self._get_memcache_timeout(timeout)
-        return self._cache.set(self.make_key(key, version=version), packed, real_timeout)
 
-    def delete(self, key, version=None):
-        self._cache.delete(self.make_key(key, version=version))
+        key = self.make_key(key, version=version)
+        return self._cache.set(key, packed, real_timeout)
 
     def get_many(self, keys, version=None):
         # First, map all of the keys through our key function
         rvals = map(lambda k: self.make_key(k, version=version), keys)
-        
+
         packed_resp = self._cache.get_multi(rvals)
-        
+
         resp = {}
         reinsert = {}
-                
+
         for key, packed in packed_resp.iteritems():
             # If it was a miss, treat it as a miss to our response & continue
             if packed is None:
                 resp[key] = packed
                 continue
-            
+
             val, refresh = self._unpack_value(packed)
             if refresh:
                 reinsert[key] = val
                 resp[key] = None
             else:
                 resp[key] = val
-        
+
         # If there are values to re-insert for a short period of time, then do
         # so now.
         if reinsert:
             self._cache.set_multi(reinsert,
                 self._get_memcache_timeout(CACHE_HERD_TIMEOUT))
-        
+
         # Build a reverse map of encoded keys to the original keys, so that
         # the returned dict's keys are what users expect (in that they match
         # what the user originally entered)
         reverse = dict(zip(rvals, keys))
-        
+
         return dict(((reverse[k], v) for k, v in resp.iteritems()))
 
-    def close(self, **kwargs):
-        self._cache.disconnect_all()
-    
-    def set_many(self, data, timeout=None, version=None, herd=True):
+    def set_many(self, data, timeout=0, version=None, herd=True):
+        safe_data = {}
         if herd and timeout != 0:
-            safe_data = dict(((self.make_key(k, version=version), self._pack_value(v, timeout))
-                for k, v in data.iteritems()))
+            for key, value in data.items():
+                key = self.make_key(key, version=version)
+                safe_data[key] = self._pack_value(value, timeout)
         else:
-            safe_data = dict((
-                (self.make_key(k, version=version), v) for k, v in data.iteritems()))
+            for key, value in data.items():
+                key = self.make_key(key, version=version)
+                safe_data[key] = value
         self._cache.set_multi(safe_data, self._get_memcache_timeout(timeout))
-    
-    def delete_many(self, keys, version=None):
-        self._cache.delete_multi(map(lambda k: self.make_key(k, version=version), keys))
-    
-    def clear(self):
-        self._cache.flush_all()
+
+class MemcachedNewCache(BaseNewCache, MemcachedCache):
+    pass
+
+class PyLibMCNewCache(BaseNewCache, PyLibMCCache):
+    @property
+    def _cache(self):
+        """
+        Overrides the default `_cache` property by allowing binary
+        protocol connections if `binary` is in the OPTIONS dict.
+        """
+        # PylibMC uses cache options as the 'behaviors' attribute.
+        # It also needs to use threadlocals, because some versions of
+        # PylibMC don't play well with the GIL.
+        client = getattr(self._local, 'client', None)
+        if client:
+            return client
+
+        if self._options and self._options.get("binary", False):
+            client = self._lib.Client(self._servers, binary=True)
+        else:
+            client = self._lib.Client(self._servers)
+
+        if self._options:
+            client.behaviors = self._options
+
+        self._local.client = client
+
+        return client
